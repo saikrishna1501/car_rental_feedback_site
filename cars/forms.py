abaa986799e02b87be3143757e2d3e789d2cc7cb
Sai Krishna Reddy Gaddam <gaddam.sai@northeastern.edu>
2024-01-07 19:42:10 -0500

    added customization through use of widgets

--- cars/forms.py
+++ cars/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class ReviewForm(forms.Form):
     first_name = forms.CharField(label="First Name", max_length=100)
     last_name = forms.CharField(label="Last Name", max_length=100)
-    email = forms.EmailField(label="Email Id")
-    review = forms.CharField(label="Review", max_length=200)
+    email = forms.EmailField(label="Email Id", widget=forms.EmailInput(attrs={'class': 'border-5px'}))
+    review = forms.CharField(label="Review", max_length=200, widget=forms.Textarea(attrs={'class': 'border-5px', 'cols': '40', 'rows': '5'}))
